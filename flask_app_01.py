6427f2bad04304a85f6cefae659dfbbc1c4457e2
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-08-15 18:42:44 +0530

    POST command

--- flask_app_01.py
+++ flask_app_01.py
@@ -24,6 +24,8 @@ db= [
   }
 ]
 
+user_jock = {"jock": "", "url": "", "id": ""}
+
 @app.get("/jock1")
 def get_jock1():
     return {"jock_1": db[0]["value"], "url": db[0]["url"]}, 200
@@ -35,14 +37,27 @@ def get_jock2():
 @app.post("/postjock")
 def post_jock():
     data = request.get_json()
-    new_jock = {
-            "jock": data["jock"],
-            "url": data["url"],
-            "id": str(uuid.uuid4().hex),
-        }
+    new_jock = {"jock": data["jock"],"url": data["url"],"id": str(uuid.uuid4().hex)}
     db.append(new_jock)
     return {"message": "jock_created"}, 201
 
+@app.post("/jock_url/<string:url>")
+def post_url(url):
+    user_jock["url"] = request.get_json()["url"]
+    db.append(user_jock)
+    return {"message": "jock added see all jocks here http://127.0.0.1:5000/jocks"}, 201
+
+@app.post("/jock/<string:jock>")
+def post_jock_only(jock):
+    user_jock["jock"] = request.get_json()["jock"]
+    user_jock["id"]= str(uuid.uuid4().hex)
+    return {"message": "jock_url requires http://127.0.0.1:5000/jock_url/<string:url>"}, 201
+
+
 @app.get("/jocks")
 def get_jocks():
+    return {"jocks": db}, 200
+
+@app.get("/")
+def get_alljocks():
     return {"jocks": db}, 200
